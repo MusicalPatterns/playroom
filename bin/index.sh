c506c5790e8e0efa91bb7fa81a871dde568a247f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-06 13:37:51 -0800

    1.0.59: third attempt at a binary for patterns

--- bin/index.sh
+++ bin/index.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-node_modules/@musical-patterns/playroom/node_modules/.bin/webpack-dev-server --config node_modules/@musical-patterns/playroom/webpack.pattern.js --color
+webpack-dev-server --config node_modules/@musical-patterns/playroom/webpack.pattern.js --color
