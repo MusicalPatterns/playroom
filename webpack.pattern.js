57e9c33bfa7ffe597fc3afc4e78d74db988d6f59
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-06 13:48:18 -0800

    1.0.62: sixth attempt at a binary for patterns

--- webpack.pattern.js
+++ webpack.pattern.js
@@ -1,6 +1,12 @@
 const dev = require('./webpack.dev')
 const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(dev, {
     entry: '../../../src/playroom.ts',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Musical Patterns - Playroom for pattern',
+        }),
+    ],
 })
