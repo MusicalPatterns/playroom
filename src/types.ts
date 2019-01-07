4f90e3221f4fdf3749cbe2999c18f84d05c15bee
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-06 20:32:17 -0800

    1.0.82: tsconfig and webpack sittin in a tree

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { ConnectedComponentClass } from 'react-redux'
-
-type ConnectedComponent<PropsType> = ConnectedComponentClass<React.FunctionComponent<PropsType>, Pick<PropsType, never>>
-
-export {
-    ConnectedComponent,
-}
