ea526938bddf3a8c7451dc023f04a682403a8f49
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-06 16:36:47 -0800

    1.0.74: fifth attempt at a binary for patterns AND lab

--- share/src/playroom.ts
+++ share/src/playroom.ts
@@ -1,17 +1,5 @@
 import { setupPlayroom } from '@musical-patterns/playroom'
-import { buildPatterns, Patterns } from '@musical-patterns/registry'
-import { pattern, patterns } from './patterns'
-
-let patternsForPlayroom: Patterns
-
-if (!patterns) {
-    patternsForPlayroom = buildPatterns({
-        [ pattern.patternId ]: pattern,
-    })
-}
-else {
-    patternsForPlayroom = patterns
-}
+import { patterns } from './patterns'
 
 const debugMode: boolean = process.env.NODE_ENV === 'development'
 
