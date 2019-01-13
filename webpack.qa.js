b5f40410249ee4e707632a9524eaaf3d653bd566
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-13 10:51:02 -0800

    eval source map for qa

--- webpack.qa.js
+++ webpack.qa.js
@@ -6,6 +6,7 @@ const path = require('path')
 
 module.exports = merge(common, {
     entry: './test/support/infrastructure/entryForTest.js',
+    devtool: 'eval-source-map',
     devServer: {
         disableHostCheck: true,
         port: 8083,
