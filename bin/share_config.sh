b44d24201ee815a44a737af3a47e0acc18eacd43
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 14:44:57 -0800

    1.0.145: to support use_latest_local for patterns, include a local library webpack config, and i think playroom should manage the build make command now as well as the build_local

--- bin/share_config.sh
+++ bin/share_config.sh
@@ -8,6 +8,7 @@ share_file Makefile.playroom
 share_file src/playroom.ts
 share_file webpack.common.js
 share_file webpack.lab.js
+share_file webpack.local.js
 share_file webpack.pattern.js
 share_file webpack.browser.js
 share_file webpack.library.js
