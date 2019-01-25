6e7e028c3258a8af1a9c55a19ce6636386cb3e44
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-25 12:52:46 -0800

    1.0.183: whoops it was my fault

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -6,5 +6,5 @@ export CLI_DIR=../../../node_modules/@musical-patterns/cli
 
 CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL=../../../node_modules/@musical-patterns/cli
 if [[ -d "${CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL}" ]] ; then
-	share_files_and_special_treatment_shared_files
+	share_files
 fi
