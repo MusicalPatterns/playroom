eae864f0d1bb5ee3ae3288b36c97f741330c3d1d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-25 20:12:06 -0800

    1.0.185: wake back up travis

--- src/playroom.ts
+++ src/playroom.ts
@@ -1,5 +1,8 @@
 import { patterns } from '@musical-patterns/pattern-playroom-test'
 import { setupPlayroom } from './index'
 
-setupPlayroom(patterns)
+const debugMode: boolean = process.env.NODE_ENV === 'development'
+
+setupPlayroom(patterns, debugMode)
     .then((playroom: HTMLDivElement) => document.body.appendChild(playroom))
+    .catch()
