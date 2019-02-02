021cecb0532ac11cae3425bec91eb1cbde9772a4
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-02 15:14:35 -0800

    debug whether in test or development

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,7 @@
 import { patterns } from '@musical-patterns/pattern-playroom-test'
 import { setupPlayroom } from './index'
 
-const debugMode: boolean = process.env.NODE_ENV === 'development'
+const debugMode: boolean = process.env.NODE_ENV !== 'production'
 
 setupPlayroom(patterns, debugMode)
     .then((playroom: HTMLDivElement) => document.body.appendChild(playroom))
