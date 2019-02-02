e6281780c8136bb889ad2e36789cfe2b0d91b8f1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-02 15:59:06 -0800

    1.0.218: shite typo

--- share/src/start.ts
+++ share/src/start.ts
@@ -1,7 +1,7 @@
 import { setupPlayroom } from '@musical-patterns/playroom'
 import { patterns } from './patterns'
 
-const debugMode: boolean = process.env.NODE_ENV !== 'development'
+const debugMode: boolean = process.env.NODE_ENV !== 'production'
 
 setupPlayroom(patterns, debugMode)
     .then((playroom: HTMLDivElement) => document.body.appendChild(playroom))
