50dc7ed29d9ef33c827124707de5b27cc5415486
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-02 14:22:12 -0800

    1.0.216: now that i dont have any insidiously silently failing typings, dont make errors that would have been caught by type system, so i can actually debug this thing

--- src/root/components/helpers/debug.ts
+++ src/root/components/helpers/debug.ts
@@ -1,22 +1,18 @@
-import {
-    calculateNoteSpecsTotalCompiledDuration,
-    compilePattern,
-    CompilePatternParameters,
-    Scale,
-} from '@musical-patterns/compiler'
-import { ThreadSpec } from '@musical-patterns/performer'
-import { logMessageToConsole, Maybe, Time } from '@musical-patterns/utilities'
+import { compilePattern, CompilePatternParameters } from '@musical-patterns/compiler'
+import { Note, ThreadSpec } from '@musical-patterns/performer'
+import { from, logMessageToConsole, Time, to } from '@musical-patterns/utilities'
 
 const logDebugInfo: (compilePatternParameters: CompilePatternParameters) => Promise<void> =
     async (compilePatternParameters: CompilePatternParameters): Promise<void> => {
         const threadSpecs: ThreadSpec[] = await compilePattern(compilePatternParameters)
         logMessageToConsole('thread specs: ', threadSpecs)
-        logMessageToConsole('compiled durations: ', threadSpecs.map((threadSpec: ThreadSpec): Time => {
-            const scales: Maybe<Scale[]> = compilePatternParameters.material.buildScalesFunction &&
-                compilePatternParameters.material.buildScalesFunction(compilePatternParameters.spec)
-
-            return calculateNoteSpecsTotalCompiledDuration(threadSpec.noteSpecs, scales)
-        }))
+        logMessageToConsole('compiled durations: ', threadSpecs.map((threadSpec: ThreadSpec): Time =>
+            threadSpec.notes ? threadSpec.notes.reduce(
+                (accumulator: Time, note: Note): Time =>
+                    to.Time(from.Time(accumulator) + from.Time(note.duration)),
+                to.Time(0),
+            ) : to.Time(0)),
+        )
     }
 
 export {
