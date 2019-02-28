442ade0bcd8dbd38ece68adb54374ecd987a7abe
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-28 15:41:17 -0800

    1.0.242: include pattern version in pattern list

--- src/pattern/components/PatternListItem.tsx
+++ src/pattern/components/PatternListItem.tsx
@@ -5,13 +5,21 @@ import { PatternListItemProps } from './types'
 
 const PatternListItem: (props: PatternListItemProps) => JSX.Element =
     ({ listedPattern, listedId, id, onClick }: PatternListItemProps): JSX.Element => {
-        const { formattedName, musicalIdeaIllustrated, mostRecentPublish, originalPublish } = listedPattern.metadata
+        const {
+            formattedName,
+            musicalIdeaIllustrated,
+            mostRecentPublish,
+            originalPublish,
+            version,
+        } = listedPattern.metadata
+
         const selectedClass: string = !id ? '' : id === listedId ? 'selected' : 'not-selected'
 
         return (
             <li {...{ className: selectedClass, id: listedId, onClick }} >
                 <div>{formattedName || constantCaseToUpperCase(listedId)}</div>
                 <div>{musicalIdeaIllustrated}</div>
+                <div>v{version}</div>
                 <div>{formatDate(originalPublish)} - {formatDate(mostRecentPublish)}</div>
             </li>
         )
