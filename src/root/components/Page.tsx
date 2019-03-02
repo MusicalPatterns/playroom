314c8d15948dd20c749ba20b5c58090979007013
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-02 10:46:52 -0800

    1.0.249: improve post and spacing

--- src/root/components/Page.tsx
+++ src/root/components/Page.tsx
@@ -13,33 +13,24 @@ const Page: (props: PageProps) => JSX.Element =
                         <div {...{ className: 'left' }}>
                             <h1>About</h1>
                             <p>
-                                Welcome to Musical Patterns.
+                                Welcome to Musical Patterns. The music here
+                                has <a {...{ href: 'https://github.com/MusicalPatterns' }}>all been written in code</a>.
+                                Each piece exists not a single listening experience but as a range of possibilities
+                                which you can explore using its controls.
                             </p>
                             <p>
-                                The music here has all been written
-                                in <a {...{ href: 'https://github.com/MusicalPatterns' }}>code</a>. Each
-                                piece exists not a single listening experience but as a range of possibilities which you
-                                can explore using its controls.
-                            </p>
-                            <p>
-                                The ideas I am exploring include spatialization (if you have a VR headset you can
-                                experience this dimension of my work), unconventional harmony, irrational rhythm, pitch
-                                and rhythm circularity, and various other illusions and ambiguities.
-                            </p>
-                            <p>
-                                I refer to the musical pieces here as "patterns" to lean
-                                - toward the aesthetics of visual texture,
-                                - toward the aesthetics of mathematics, and
-                                - away from the aesthetics of songwriting.
-                            </p>
-                            <p>
-                                They're brief and repeat indefinitely, so listen inwards.
+                                I refer to the musical pieces here as "patterns" to lean toward the aesthetics of visual
+                                texture, toward the aesthetics of mathematics, and away from the aesthetics of
+                                songwriting. They're brief and repeat indefinitely. The ideas I am exploring include
+                                spatialization (if you have a VR headset you can experience this dimension of my work),
+                                unconventional harmony, irrational rhythm, pitch and rhythm circularity, and various
+                                other illusions and ambiguities.
                             </p>
                             <h3>
                                 test-driven composition
                             </h3>
                             <p>
-                                For this project I have used a software development technique called test-driven
+                                For this project I have followed a software development practice called test-driven
                                 development, or TDD. What this means is that I begin by describing new behavior I want
                                 in the form of an automated test (for example, I might write a test that expects every
                                 melodic interval to lay within a particular harmonic complexity threshold). The test
@@ -47,7 +38,7 @@ const Page: (props: PageProps) => JSX.Element =
                                 But then I work on the code for the music itself until the test passes.
                             </p>
                             <p>
-                                A benefit of this approach is that once the desired musical behavior is achieved, it is
+                                A benefit of this technique is that once the desired musical behavior is achieved, it is
                                 thenceforth protected by this test; I can continue to add new layers of complexity to
                                 the piece, and if any new feature breaks this existing one, the test will fail, alerting
                                 me to reconsider my approach. The test also serves as living documentation of my earlier
@@ -68,44 +59,46 @@ const Page: (props: PageProps) => JSX.Element =
                                 it to my own music, I've come to believe that the practice has potential to inspire the
                                 field of music composition. Certainly it's not for everybody, but it is capable of
                                 breeding an intriguing (dare I say original) musical aesthetic. Music written in code is
-                                fairly commonplace, and already boasts a distinct aesthetic, but test-driven music goes
+                                commonplace, and already boasts a distinct aesthetic, but test-driven music goes
                                 beyond even that.
                             </p>
                             <p>
-                                Allow me to review the aesthetics of music written in code. It tends to sound less
-                                song-like, and that's a result of the composing process. Compare it with traditional
-                                composition. When you start with a blank page of sheet music, you kind of have to start
-                                at the beginning, at least the beginning of some section. Writing music in code, though,
-                                you could start by engineering a musical property which is distributed throughout the
-                                piece; you could spend a whole morning and accomplish a big chunk of your work and still
-                                not yet have any final material that could actually be heard. It's more like designing a
-                                wallpaper pattern, starting with a basic outline, filling in with increasing detail over
-                                time. It's working inwards. It's not as intuitive.
+                                First allow me to review the aesthetics of music written in code. It tends to sound less
+                                song-like, and that's a direct result of the composing process. Compare it with
+                                traditional composition: when you start with a blank page of sheet music, you kind of
+                                have to start at the beginning, at least the beginning of some section. Writing music in
+                                code, though, you could start by engineering a musical property which is distributed
+                                throughout the piece; you could spend a whole morning and accomplish a big chunk of your
+                                work and still not yet have any final material that could actually be heard. It's more
+                                like designing a wallpaper pattern, starting with a basic outline, filling in with
+                                increasing detail over time. It's working inwards rather than across, even though one
+                                always in the end listens to the music across, so it's less intuitive.
                             </p>
                             <p>
-                                Test-driving music is similar, but further. It allows you to work even less intuitively.
-                                The tests afford you to pick back up after several months of inactivity. For the past
-                                few years I would build the occasional little musical experiment in code but did not
-                                test-drive it. Whenever I would try to pick one of them back up, I would find that I'd
-                                lost too much context on the purposes for many of its components, and didn't know how to
-                                contribute any further to it. I learned to think of my code-based music in a certain
-                                way: I felt pressured to start and complete a composition in a single sitting (for some
-                                definition of sitting), because I knew once I stepped away, I may as well start from
-                                scratch if I had ideas for revisions later. Well now I don't have to think like that.
-                                I'm composing a dozen pieces at once. I keep having little ideas for tweaks to various
-                                of time over time. I occasionally discover surprising connections between them. And when
-                                the place you were at before has faded from your mind, it's actually a feature, not a
-                                bug! It's an injection of a fresh new perspective into things. Of course it's still your
-                                perspective, but it's a new you.
-                            </p>
-                            <p>
-                                I don't write outwards, I write inwards. And now I can do that on another scale.
+                                Test-driving music allows you to work even less intuitively, which leads to even less
+                                familiar-sounding, less song-like music. The tests enable you to pick back up after
+                                several months of inactivity. For the past few years I would build the occasional little
+                                musical experiment in code but did not test-drive it. Whenever I would try to pick one
+                                of them back up, I found I'd lost too much context on the components of the code, and I
+                                didn't know how to contribute any further to it. I learned to feel pressured to start
+                                and finish a composition in a single sitting (for some definition of sitting), because I
+                                knew once I stepped away, I might as well start from scratch if I had ideas for
+                                revisions later. Using TDD, though, I don't have to constrain myself like that.
+                                I can be composing a dozen pieces at once, asynchronously, tweaking them bit by bit over
+                                long periods of time. I helps me discover surprising connections between them. Also,
+                                when the mental place you were at before has faded from your mind, it's actually a
+                                feature, not a bug! It's an injection of a fresh new perspective into things. Of course
+                                it's still your perspective, but it's a new you.
                             </p>
                             <p>
                                 Test-driving has other benefits, too, which contribute to the aesthetic. Forcing
                                 yourself to describe the behavior you want first in the form of a test ensures you think
                                 critically and carefully about what you're doing, rather than just slapping something
-                                together, QA'ing it with your ears and shipping. Also, by following a short cycle of
+                                together, QA'ing it with your ears and shipping. You don't really understand something
+                                until you can explain it a couple ways, and that's true even of your own artistic
+                                impulses; challenging yourself to explain what you want before going for it often leads
+                                to answering questions about your desires which hadn't yet even occurred to you, leading
+                                to new variants and musical developments. Also, by following a short cycle of
                                 testing, implementing, testing, implementing, it reduces the tendency to get fanciful,
                                 adding anticipatory bells and whistles which aren't subservient to the immediate and
                                 minimum satisfaction of the goal you laid out in your latest test.
@@ -114,8 +107,8 @@ const Page: (props: PageProps) => JSX.Element =
                                 Test-driving music is so suited to the way I approach composing myself that it has
                                 revolutionized my musical creativity. If others are out there who approach composition
                                 similarly to me, or who might benefit from experimenting with such approaches, perhaps
-                                TDD would be worth a shot. Actually, the page about unit testing in the wiki for the
-                                SuperCollider platform
+                                TDD would be worth a shot. I just discovered that the page about unit testing in the
+                                wiki for the SuperCollider platform
                                 includes <a {...{ href: 'https://github.com/supercollider/supercollider/wiki/Unit-Testing-Guide#test-driven-development-redgreen-testing' }}>a
                                 section about TDD</a>, so maybe it's not too far on the horizon.
                             </p>
@@ -124,8 +117,10 @@ const Page: (props: PageProps) => JSX.Element =
                             </h3>
                             <p>
                                 One of my goals with this project was to consolidate my musical work under a single
-                                system. Abstracting features across the patterns breeds creativity and builds me toward
-                                something greater. Essentially I am developing my own specialized musical instrument.
+                                system. Challenging myself to keep my code clean by abstracting features across my
+                                patterns is another breeding ground for creativity, as well as means for discovery of
+                                interconnectedness between my musical thoughts, which can lead me towards greater
+                                unified compositions. Essentially I am developing my own specialized musical instrument.
                             </p>
                             <p>
                                 I nonetheless consider this output to be proof-of-concept. Electronic music production
@@ -168,7 +163,7 @@ const Page: (props: PageProps) => JSX.Element =
                                 Douglas Blumeyer, March 2019
                             </p>
                         </div>
-                        <div{...{ className: 'right closed' }}/>
+                        <div {...{ className: 'right closed' }}/>
                     </div>
                 )
             default:
